57d7e57287ef7f5fc68422559ce7ba0c82ed61ef
Randhyll <randhallr@portobellosouth.com>
2022-01-30 21:53:53 -0300

    Fix Get Order Shop All Query

--- OrderShopApi/src/OrderShopNet.Api.Application/Order/Queries/GetAll/GetOrderShopAllQuery.cs
+++ OrderShopApi/src/OrderShopNet.Api.Application/Order/Queries/GetAll/GetOrderShopAllQuery.cs
@@ -5,35 +5,34 @@ using Microsoft.EntityFrameworkCore;
 using OrderShopNet.Api.Application.Common.Interfaces;
 using OrderShopNet.Api.Application.EntitiesDto;
 
-namespace OrderShopNet.Api.Application.Order.Queries.GetAll
+namespace OrderShopNet.Api.Application.Order.Queries.GetAll;
+
+public sealed class GetOrderShopAllQuery : IRequest<GetAllVm>
 {
-    public sealed class GetOrderShopAllQuery : IRequest<GetAllVm>
+}
+
+public sealed class GetOrderShopAllQueryHandler : IRequestHandler<GetOrderShopAllQuery, GetAllVm>
+{
+    private readonly IApplicationDbContext context;
+    private readonly IMapper mapper;
+
+    public GetOrderShopAllQueryHandler(IApplicationDbContext _context, IMapper _mapper)
     {
+        this.context = _context ?? throw new ArgumentNullException(nameof(_context));
+        this.mapper = _mapper ?? throw new ArgumentNullException(nameof(_mapper));
     }
 
-    public sealed class GetOrderShopAllQueryHandler : IRequestHandler<GetOrderShopAllQuery, GetAllVm>
+    public async Task<GetAllVm> Handle(GetOrderShopAllQuery request, CancellationToken cancellationToken)
     {
-        private readonly IApplicationDbContext context;
-        private readonly IMapper mapper;
-
-        public GetOrderShopAllQueryHandler(IApplicationDbContext _context, IMapper _mapper)
+        var listVm = new GetAllVm
         {
-            this.context = _context ?? throw new ArgumentNullException(nameof(_context));
-            this.mapper = _mapper ?? throw new ArgumentNullException(nameof(_mapper));
-        }
+            Lists = await context.OrderShops
+            .AsNoTracking()
+            .ProjectTo<OrderShopDto>(mapper.ConfigurationProvider)
 
-        public async Task<GetAllVm> Handle(GetOrderShopAllQuery request, CancellationToken cancellationToken)
-        {            
-            var listVm = new GetAllVm
-            {               
-                Lists = await context.OrderShops
-                .AsNoTracking()
-                .ProjectTo<OrderShopDto>(mapper.ConfigurationProvider)
-                
-                .OrderBy(t => t.Title)                
-                .ToListAsync(cancellationToken)
-            };
-            return listVm;
-        }
+            .OrderBy(t => t.Title)
+            .ToListAsync(cancellationToken)
+        };
+        return listVm;
     }
 }
